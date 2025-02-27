a5a476fb0919b80f59c9f0f9f617e42a27458615
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-02-27 12:49:12 -0500

    2/7

--- src/main/java/ThreeSum_BruteForceAlgorithm.java
+++ src/main/java/ThreeSum_BruteForceAlgorithm.java
@@ -2,15 +2,12 @@ public class ThreeSum_BruteForceAlgorithm {
     public static int count(int[] nums) {
         int n = nums.length;
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                for (int k = j + 1; k < n; k++)
                     if (nums[i] + nums[j] + nums[k] == 0) {
                         count++;
                     }
-                }
-            }
-        }
 
         return count;
     }
