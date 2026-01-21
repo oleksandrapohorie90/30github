cb90cace22982cab86b48f4abf52b38a59929259
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2026-01-20 21:56:29 -0500

    1/20/2026

---
+++ src/main/java/oop_january/LoginPage.java
@@ -0,0 +1,27 @@
+package oop_january;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+public class LoginPage {
+
+    private final WebDriver driver;
+
+    //represents the UI
+    private By usernameInput = By.id("username");
+    private By passwordInput = By.id("password");
+    private By loginButton = By.id("loginBtn");
+
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void login(User user) {
+        driver.findElement(usernameInput).sendKeys(user.getUsername());
+        driver.findElement(passwordInput).sendKeys(user.getPassword());
+        driver.findElement(loginButton).click();
+    }
+
+
+}
